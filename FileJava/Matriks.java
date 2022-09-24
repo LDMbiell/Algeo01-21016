f377b2f0e54ac8c929ef338c5a1e52204856fddc
LDMbiell <110374011+LDMbiell@users.noreply.github.com>
2022-09-24 17:29:00 +0700

    penting nyoba dulu :)

---
+++ FileJava/Matriks.java
@@ -0,0 +1,8 @@
+import java.util.*;
+//https://www.tutorialspoint.com/What-does-import-Java-util-in-Java-do
+import java.io.*;
+//https://www.geeksforgeeks.org/java-io-input-output-in-java-with-examples/
+
+public class Matriks{
+}
+//fungsi definisi matriks
