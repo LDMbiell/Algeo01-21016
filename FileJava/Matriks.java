f4225c1054e79f062ac3cd7ed47a82b22e2a6689
LDMbiell <110374011+LDMbiell@users.noreply.github.com>
2022-09-25 17:23:16 +0700

    Update Matriks.java

--- FileJava/Matriks.java
+++ FileJava/Matriks.java
@@ -2,9 +2,6 @@ import java.util.*;
 //https://www.tutorialspoint.com/What-does-import-Java-util-in-Java-do
 import java.io.*;
 //https://www.geeksforgeeks.org/java-io-input-output-in-java-with-examples/
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 
 public class Matriks{
@@ -12,9 +9,6 @@ public class Matriks{
   
   /* ********** PENDEFINISIAN ********** */
   
-  
-  InputStreamReader isr = new InputStreamReader(System.in);
-  BufferedReader scanner = new BufferedReader(isr);
   Scanner scanner = new Scanner(System.in); // membuat input 
     public int MinimalKolom = 0; //mendefinisikan konstanta minimal kolom
     public int MinimalBaris = 0; //mendefinisikan konstanta minimal baris
@@ -23,7 +17,7 @@ public class Matriks{
     public int Baris; // mendefinisikan Baris matriks dengan Baris
     public int Kolom; // mendefinisikan Kolom matriks dengan Kolom
     public double[][] Matrix; // mendefinisikan isi array, jenisnya double agar threadsafe
-    // cara untuk memanggil isi matriks-nya matriks.Matrix[Baris][Kolom]
+    // cara untuk memanggil isi matriks-nya yaitu matriks.Matrix[Baris][Kolom]
     // Indeks Baris & Kolom mulai dari 0 sesuai MinimalBaris dan MinimalKolom
   
   /* ********** KONSTRUKTOR ********** */
