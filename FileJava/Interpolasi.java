9ea18e40ea7336bf76dfadee4c86b5af702b365d
Lailabkn <110519274+Lailabkn@users.noreply.github.com>
2022-10-03 20:17:38 +0700

    Update Interpolasi.java

--- FileJava/Interpolasi.java
+++ FileJava/Interpolasi.java
@@ -1,14 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-public class Interpolasi {
-  //Membaca matriks menggunakan file
-  public static void Input(String nama_file, Matriks Min) throws FileNotFoundException { 
+public class Interpolation {
+
+    public static void Input(String file_name, Matriks Min) throws FileNotFoundException { // Membaca Matriks dari
+                                                                                           // sebuah file
         ArrayList<ArrayList<Double>> Mat = new ArrayList<ArrayList<Double>>();
-        File file = new File(nama_file);
+        File file = new File(file_name);
         Scanner input = new Scanner(file);
-        int Kolom = -1;
         int Baris = -1;
+        int Kolom = -1;
         while (input.hasNextLine()) {
             Baris++;
             Mat.add(new ArrayList<Double>());
@@ -21,54 +22,55 @@ public class Interpolasi {
         }
 
         if (Baris == 0) {
-            System.out.println("Tidak dapat membaca file yang diinput");
+            System.out.println("Tidak dapat membaca file");
         } else {
             Kolom = Mat.get(0).size();
-            Min.Mat = new double[Mat.size()][Mat.get(0).size()];
+            Min.Matrix = new double[Mat.size()][Mat.get(0).size()];
             for (int i = 0; i <= Baris; i++) {
                 for (int j = 0; j < Kolom; j++) {
-                    Min.Mat[i][j] = Mat.get(i).get(j);
+                    Min.Matrix[i][j] = Mat.get(i).get(j);
                 }
             }
             Min.Baris = Baris + 1;
             Min.Kolom = Kolom;
         }
     }
-  
-  
-  public static void Cari() {
-        int spl = Menu.menuInput();
+
+    public static void Cari() {
+        int inputspl = Menu.menuInput();
         Matriks M = new Matriks(1, 1);
-        if (spl == 1) {
+        if (inputspl == 1) {
 
             Scanner scanner = new Scanner(System.in);
             System.out.print("Masukan n : ");
             int n = scanner.nextInt();
             M = new Matriks(n, 2);
 
-            M.BacaMat();
-        } else if (spl == 2) {
-            String nama_file = Menu.inputFile();
+            M.BacaMatriks();
+        } else if (inputspl == 2) {
+            String filename = Menu.inputFile();
             try {
-                Input(nama_file, M);
-                M.TulisMat();
+                Input(filename, M);
+                M.TulisMatriks();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
+
         int n = M.Baris;
 
-        // untuk memisahkan input
+        // SPLIT INPUT
         Matriks A = new Matriks(n, n + 1);
         for (int i = A.GetFirstIdxBrs(A); i <= A.GetLastIdxBrs(A); i++) {
             for (int j = A.GetFirstIdxKol(A); j <= A.GetLastIdxKol(A) - 1; j++) {
-                A.Mat[i][j] = Math.pow(M.Mat[i][0], j);
+                A.Matrix[i][j] = Math.pow(M.Matrix[i][0], j);
             }
         }
         for (int i = A.GetFirstIdxBrs(A); i <= A.GetLastIdxBrs(A); i++) {
-            A.Mat[i][n] = M.Mat[i][1];
+            A.Matrix[i][n] = M.Matrix[i][1];
         }
 
         SPL.SPLGauss(A, "a");
+
     }
 }
